819bc9742dc13f442ec67df285c297287078bd16
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-02-10 00:52:17 +0300

    Add solution for Car Pooling

--- camp/leetcode/car-pooling.py
+++ camp/leetcode/car-pooling.py
@@ -1,20 +1,14 @@
-class Solution(object):
-    def carPooling(self, trips, capacity):
-        """
-        :type trips: List[List[int]]
-        :type capacity: int
-        :rtype: bool
-        """
-        range_sum = [0 for _ in range(1001)]
-        for numPassengers, start, end in trips:
-            range_sum[start] += numPassengers
-            range_sum[end] -= numPassengers
-        
-        accumulator = 0
-        for num in range_sum:
-            accumulator += num
-            if accumulator > capacity:
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        passengers = [0 for i in range(1001)]
+        for num_passengers, start, end in trips:
+            passengers[start] += num_passengers
+            passengers[end] -= num_passengers
+
+        total_count = 0
+        for num_passengers in passengers:
+            total_count += num_passengers
+            if total_count > capacity:
                 return False
-        
+
         return True
-        
