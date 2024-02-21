866bb05cc809b64b5f07ecb9c5482607d52f75be
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-02-21 12:14:23 +0300

    Add solution for Merge Two Sorted Lists

--- camp/leetcode/merge-two-sorted-lists.py
+++ camp/leetcode/merge-two-sorted-lists.py
@@ -1,26 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+class Solution(object):
+    def mergeTwoLists(self, list1, list2, head = None, tail = None):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
         if not list1 and not list2:
-            return None
-            
-        merged = ListNode()
-        node = merged
-        while list1 or list2:
-            if not list2 or list1 and list1.val < list2.val:
-                node.val = list1.val
-                list1 = list1.next
+            return head
+        elif not list2 or list1 and list1.val < list2.val:
+            temp = list1.next
+            list1.next = None
+            if head:
+                tail.next = list1
+                tail = list1
             else:
-                node.val = list2.val
-                list2 = list2.next
-            if list1 or list2:
-                node.next = ListNode()
-                node = node.next
+                head = list1
+                tail = list1
+            return self.mergeTwoLists(temp, list2, head, tail)
+        else:
+            temp = list2.next
+            list2.next = None
+            if head:
+                tail.next = list2
+                tail = list2
+            else:
+                head = list2
+                tail = list2
+            return self.mergeTwoLists(list1, temp, head, tail)
         
-        return merged
-
+        
         
