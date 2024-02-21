71f8509cb2a017d77d9457f09a97f1505b688088
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-02-21 05:43:01 +0300

    Add solution for Dota2 Senate

---
+++ camp/leetcode/dota2-senate.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def predictPartyVictory(self, senate):
+        """
+        :type senate: str
+        :rtype: str
+        """
+        radiants = deque()
+        dires = deque()
+        for i, party in enumerate(senate):
+            if party == "R":
+                radiants.append(i)
+            else:
+                dires.append(i)
+
+        print(radiants)
+        print(dires)
+
+        i = 0
+        n = len(senate)
+        while radiants or dires:
+            i = i % n
+            party = senate[i]
+            if party == "R" and radiants and radiants[0] == i:
+                if not dires:
+                    return "Radiant"
+                else:
+                    dires.popleft()
+                    radiants.append(radiants.popleft())
+            elif party == "D" and dires and dires[0] == i:
+                if not radiants:
+                    return "Dire"
+                else:
+                    radiants.popleft()
+                    dires.append(dires.popleft())
+            i += 1
+            
+
+
+        
