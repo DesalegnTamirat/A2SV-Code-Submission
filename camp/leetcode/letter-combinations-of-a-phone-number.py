b21bbbbd792f7899a9a6ac2c09865ab153695a90
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-03-01 14:53:55 +0300

    Add solution for Letter Combinations of a Phone Number

--- camp/leetcode/letter-combinations-of-a-phone-number.py
+++ camp/leetcode/letter-combinations-of-a-phone-number.py
@@ -2,26 +2,20 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        digit_letters = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-        
-        possibilities = []
-        for digit in digits:
-            possibilities.append(digit_letters[digit])
-        
-        answer = []
-        path = []
-        def backtrack(i):
-            if len(path) == len(digits):
-                answer.append("".join(path))
-                return
-            if i == len(possibilities):
-                return
 
-            for let in possibilities[i]:
-                path.append(let)
+        digit_letters = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+        combinations = []
+        letters = []
+        def backtrack(i):
+            if len(letters) == len(digits):
+                combinations.append("".join(letters))
+                return 
+            
+            for letter in digit_letters[digits[i]]:
+                letters.append(letter)
                 backtrack(i + 1)
-                path.pop()
-                backtrack(i + 1)
+                letters.pop()
             
         backtrack(0)
-        return answer
+        return combinations
