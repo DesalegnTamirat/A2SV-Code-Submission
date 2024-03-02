e3bd5dcbc27f155f9d7a2edfb13479231cb86d38
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-03-02 06:45:59 +0300

    Add solution for N-Queens II

---
+++ camp/leetcode/n-queens-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cols = set()
+        major_diagonal = set()
+        minor_diagonal = set()
+        board = [["."]* n for _ in range(n)]
+        result = []
+        def backtrack(row):
+            if row == n:
+                result.append(["".join(cell) for cell in board])
+                return
+            
+            for col in range(n):
+                if col in cols:
+                    continue
+                if (row + col) in major_diagonal:
+                    continue
+                if (row - col) in minor_diagonal:
+                    continue
+                
+                cols.add(col)
+                major_diagonal.add(row + col)
+                minor_diagonal.add(row - col)
+                board[row][col] = "Q"
+
+                backtrack(row + 1)
+
+                cols.remove(col)
+                major_diagonal.remove(row + col)
+                minor_diagonal.remove(row - col)
+                board[row][col] = "."
+
+        backtrack(0)
+        print(result)
+        return len(result)
