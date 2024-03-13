7f99396c57f501e4158bd2d1d8a12d4a7978d2bb
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-03-13 23:12:26 +0300

    Add solution for Find First and Last Position of Element in Sorted Array

--- camp/leetcode/find-first-and-last-position-of-element-in-sorted-array.py
+++ camp/leetcode/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,35 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [self.starting(nums, target), self.ending(nums, target)]
+        def find_first(nums, target):
+            left = 0
+            right = len(nums) - 1
+            answer = -1
+            while left <= right:
+                middle = (left + right) // 2
+                if nums[middle] == target:
+                    answer = middle
+                
+                if nums[middle] >= target:
+                    right = middle - 1
+                else:
+                    left = middle + 1
 
-    def starting(self, nums, target):
-        start, end = 0, len(nums) - 1
-        first = -1
-        while start <= end:
-            mid = (start + end) // 2
-            if nums[mid] == target:
-                first = mid
-                end = mid - 1
-            elif nums[mid] > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-
-        return first
-
-    def ending(self, nums, target):
-        last = -1
-        start, end = 0, len(nums) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if nums[mid] == target:
-                last = mid
-                start = mid + 1
-            elif nums[mid] < target:
-                start = mid + 1
-            else:
-                end = mid - 1
+            return answer
         
-        return last
+        def find_last(nums, target):
+            answer = -1
+            left = 0
+            right = len(nums) - 1
+            while left <= right:
+                middle = (left + right) // 2
+                if nums[middle] == target:
+                    answer = middle
+                
+                if nums[middle] <= target:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+            
+            return answer
+        
+        return [find_first(nums, target), find_last(nums, target)]
